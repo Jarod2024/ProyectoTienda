0a310a06d4d38d23aec48425294f8787f51fb8e6
FernandoQ25 <romelquinapallo10@gmail.com>
2024-07-02 12:56:11 -0500

    Solucion del archivo composer.lock

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+Route::get('/', function () {
+    return view('home');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/profile', [ProfileController::class, 'show'])->name('profile');
+Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
