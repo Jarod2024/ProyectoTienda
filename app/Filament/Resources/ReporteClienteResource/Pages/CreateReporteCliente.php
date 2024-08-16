527f03230f5c65a934b73a39374747b3ce81d671
Jarod2024 <jsguallichico1@espe.edu.ec>
2024-08-16 09:22:54 -0500

    Reportes de cliente, producto y comprobante

---
+++ app/Filament/Resources/ReporteClienteResource/Pages/CreateReporteCliente.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ReporteClienteResource\Pages;
+
+use App\Filament\Resources\ReporteClienteResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateReporteCliente extends CreateRecord
+{
+    protected static string $resource = ReporteClienteResource::class;
+}
---
+++ resources/views/PDF/ProductosPDF.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Reporte de Productos</title>
+    <!-- Enlace a Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f9fa;
+            padding: 20px;
+        }
+        .header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 40px;
+            margin-right:40px;
+        }
+        .store-info {
+            text-align: left;
+        }
+        .store-info h2 {
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: 5px;
+        }
+        .store-info p {
+            margin: 0;
+            font-size: 15px;
+        }
+        .header img {
+            max-width: 150px;
+        }
+        h1 {
+            text-align: left;
+            margin-bottom: 30px;
+            font-size: 20px;
+            color: #343a40;
+            padding-top: 50px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        th, td {
+            padding: 12px;
+            text-align: left;
+            font-size:15px;
+        }
+        th {
+            background-color: #007bff;
+            color: #fff;
+            font-weight: bold;
+        }
+        tbody tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        tbody tr:hover {
+            background-color: #e9ecef;
+        }
+        
+
+    </style>
+</head>
+<body>
+    
+<div style="width: 100%; overflow: visible;margin-bottom:165px">
+    <div style="float: left; width: 70%; font-size: 14px; line-height: 1.2;">
+       <br> <h2 style="margin: 0;">Nombre de la Tienda</h2>
+        <p style="margin: 5px 0;">Dirección de la Tienda</p>
+        <p style="margin: 5px 0;">Teléfono: +123 456 7890</p>
+        <p style="margin: 5px 0;">Email: contacto@tienda.com</p>
+    </div>
+    <div style="float: right; width: 25%;">
+        <img src="{{ public_path('images/Logo3.jpg') }}" alt="Logo de la Tienda" style="max-width: 100%; height: auto;">
+    </div>
+</div>
+
+
+
+
+
+    <h1>Reporte de Productos</h1>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Descipcion</th>
+                <th>Precio</th>
+                <th>Plataforma</th>
+                <th>Categoria</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($reporteProductos as $producto)
+                <tr>
+                    <td>{{ $producto->id }}</td>
+                    <td>{{ $producto->nombre }}</td>
+                    <td>{{ $producto->descripcion }}</td>
+                    <td>{{ $producto->precio }}</td>
+                    <td>{{$producto->plataforma->nombre }}</td>
+                    <td>{{$producto->categoria->nombre  }}</td>
+                    
+
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <!-- Enlace a Bootstrap JS (opcional si necesitas funcionalidades JS de Bootstrap) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
